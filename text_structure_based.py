4b3e9d593567584edfbe2c53429d37b479a2ec8f
GODASI SAI AMMANNANAIDU <148623717+Godasisai@users.noreply.github.com>
2026-07-14 21:33:49 +0530

    Add files via upload

---
+++ text_structure_based.py
@@ -0,0 +1,23 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text = """
+Space exploration has led to incredible scientific discoveries. From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what’s possible beyond our planet.
+
+These missions have not only expanded our knowledge of the universe but have also contributed to advancements in technology here on Earth. Satellite communications, GPS, and even certain medical imaging techniques trace their roots back to innovations driven by space programs.
+"""
+
+# Create the splitter
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=20,
+)
+
+# Split the text
+chunks = splitter.split_text(text)
+
+# Print the chunks
+print("Total Chunks:", len(chunks))
+
+for i, chunk in enumerate(chunks, start=1):
+    print(f"\nChunk {i}:")
+    print(chunk)
